ea158afd733c4b78d6dbe6bdfda8d0e339ff3c90
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2025-11-22 18:00:40 +0000

    fix: minor issue

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ app.add_middleware(
 )
 # --------------------------------------------------
 
-@app.on_event("startup")
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+# @app.on_event("startup")
+# async def init_db():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
 
 app.include_router(auth_router)
 
