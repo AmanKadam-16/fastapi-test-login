ceffad4f4dbbb7af93d54cb81cd6a814c6bd7eb9
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2025-11-22 18:10:48 +0000

    "Fix ASGI entrypoint and Procfile"

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
-from fastapi import FastAPI
-from src.test_login.auth.routes import router as auth_router
-from src.test_login.models import Base
-from src.test_login.database import engine
-from fastapi.middleware.cors import CORSMiddleware
+from src.test_login.main import app
 
-app = FastAPI(title="ElectroSoft Auth API")
-
-# --------------------------------------------------
-# GLOBAL CORS (ALLOW ALL)
-# --------------------------------------------------
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],       # allow all domains
-    allow_credentials=True,
-    allow_methods=["*"],       # allow GET, POST, PUT, DELETE, etc.
-    allow_headers=["*"],       # allow all headers including Authorization
-)
-# --------------------------------------------------
-
-# @app.on_event("startup")
-# async def init_db():
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-
-app.include_router(auth_router)
-
-
-@app.get("/")
-def home():
-    return {"message": "ElectroSoft API running"}
+# Optional: If running locally
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
