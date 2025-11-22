e08688de0e9678593db8489ff25aa48bf5803701
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2025-11-22 17:29:31 +0000

    fix: allow cors

--- test-login/src/test_login/main.py
+++ test-login/src/test_login/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from src.test_login.auth.routes import router as auth_router
 from src.test_login.models import Base
 from src.test_login.database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ElectroSoft Auth API")
+
+# --------------------------------------------------
+# GLOBAL CORS (ALLOW ALL)
+# --------------------------------------------------
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],       # allow all domains
+    allow_credentials=True,
+    allow_methods=["*"],       # allow GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],       # allow all headers including Authorization
+)
+# --------------------------------------------------
 
 @app.on_event("startup")
 async def init_db():
