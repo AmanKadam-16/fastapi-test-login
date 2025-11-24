c36115c839ac83a60f78df83f231aa5a0a0056a5
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2025-11-24 15:39:29 +0530

    reduce access token expiry in minutes

--- src/test_login/config.py
+++ src/test_login/config.py
@@ -7,7 +7,7 @@ class Settings:
     DB_URL: str = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./app.db")  # neon connection string
     JWT_SECRET: str = os.getenv("JWT_SECRET", "supersecret")
     JWT_REFRESH_SECRET: str = os.getenv("JWT_REFRESH_SECRET", "superrefresh")
-    ACCESS_EXPIRE_MINUTES: int = 15
+    ACCESS_EXPIRE_MINUTES: int = 1
     REFRESH_EXPIRE_DAYS: int = 30
 
 settings = Settings()
